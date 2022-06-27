53466f9d7d8310921ff035cac2cf51829560743e
ManbirK <72926520+ManbirK@users.noreply.github.com>
2022-06-27 18:56:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,12 +8,42 @@
 
   camera = document.getElementById("camera");
 
-Webcam.attach( '#camera' );
+  Webcam.attach( '#camera' );
 
-      
-function take_snapshot()
-{
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
-    });
+
+  function take_snapshot()
+  {
+      Webcam.snap(function(snapshot) {
+          document.getElementById("result").innerHTML = '<img id="captured_image" src="'+  snapshot+'"/>';
+      });
+  }
+
+console.log("Ml5 Version ", ml5.version);
+
+my_classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/9NM-3OxOJ/model.json", modelLoaded);
+
+function modelLoaded(){
+
+  console.log("Model is Loaded");
+}
+  
+function check(){
+
+  my_img = document.getElementById("captured_image");
+  my_classifier.classify(my_img, gotresults);  
+
+
+}
+
+function gotresults(error, results){
+
+  if (error){
+    console.error(error);
+  }
+  else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = (results[0].confidence * 100).toFixed(2) + "%";
+  }
+
 }
